178a768dc90c7fb3724be2558c216d8f612c991c
Paulo Alenquer <pauloalenquer@gmail.com>
2022-07-27 18:06:11 -0300

    case done

--- lib/desafio_stone.ex
+++ lib/desafio_stone.ex
@@ -37,28 +37,50 @@ defmodule DesafioStone do
 
   defp divide_values(value, emails) do
     emails_length = length(emails)
-    IO.puts(rem(value, emails_length))
-    div(value, emails_length)
+    div_total = Enum.map(emails, fn _ -> %{value: div(value, emails_length)} end)
+    check_rem = rem(value, emails_length)
+
+    if check_rem > 0 do
+      sum_rem(div_total, 0, check_rem)
+    else
+      div_total
+    end
   end
 
-  defp total(value, emails),
-    do: {:ok, Enum.map(emails, fn email -> %{email: email, value: value} end)}
+  defp sum_rem(list, _, 0) do
+    list
+  end
+
+  defp sum_rem([head | tail], index, value) do
+    new_head = %{value: head.value + 1}
+
+    [new_head | sum_rem(tail, index + 1, value - 1)]
+  end
+
+  defp total(values, emails) do
+    Enum.zip(values, Enum.map(emails, fn email -> %{email: email} end))
+  end
 end
 
 mock_itens = [
   %{
     item: "Banana",
     amount: 1,
-    price: 52
+    price: 33
   },
   %{
     item: "Laranja",
     amount: 1,
-    price: 50
+    price: 33
+  },
+  %{
+    item: "Laranja",
+    amount: 1,
+    price: 35
   }
 ]
 
-mock_emails = ["teste@teste.com", "stone@teste.com", "teste@stone.com", "stone@stone.com"]
+mock_emails = ["teste@teste.com", "stone@teste.com", "teste@stone.com"]
 
 handle_list = DesafioStone.receive_list(mock_itens, mock_emails)
 
